e7a969973cce8b0bd8489f29c845536fa9a204f0
andres <davidroa8@hotmail.com>
2022-07-28 16:28:30 -0500

    app microservices

---
+++ microservicio_imagen/src/main/java/com/microservicio/microservicio_imagen/MicroservicioImagenApplication.java
@@ -0,0 +1,13 @@
+package com.microservicio.microservicio_imagen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioImagenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioImagenApplication.class, args);
+	}
+
+}
---
+++ microservicio_persona/src/main/java/com/backend/microservicio_persona/MicroservicioPersonaApplication.java
@@ -0,0 +1,13 @@
+package com.backend.microservicio_persona;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioPersonaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioPersonaApplication.class, args);
+	}
+
+}
