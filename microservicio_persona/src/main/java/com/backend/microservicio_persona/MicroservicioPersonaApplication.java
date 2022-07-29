005269485c251a02e012ea21066d4420306c9244
andres <davidroa8@hotmail.com>
2022-07-29 12:55:11 -0500

    Actualización properties y eureka server

--- microservicio_persona/src/main/java/com/backend/microservicio_persona/MicroservicioPersonaApplication.java
+++ microservicio_persona/src/main/java/com/backend/microservicio_persona/MicroservicioPersonaApplication.java
@@ -2,7 +2,8 @@ package com.backend.microservicio_persona;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+@EnableEurekaClient
 @SpringBootApplication
 public class MicroservicioPersonaApplication {
 
